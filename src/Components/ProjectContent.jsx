43b90a375d897e25b52c898e081d391518772e47
Killian D.B. <115996000+KillianDB@users.noreply.github.com>
2025-02-06 19:35:08 -0300

    Update ProjectContent.jsx

--- src/Components/ProjectContent.jsx
+++ src/Components/ProjectContent.jsx
@@ -11,34 +11,10 @@ function ProjectContent({project}) {
                                  <div class="info-content"> 
                                      <p class="h1 titleProject">{project.title}</p> 
                                      <p class="h6 descriptionProject"> 
-                                         Project.description}
-                                     </p> 
-                                     <ul class="technologiesProject"> 
-                                       {for(i=0;i>project.legth(); i++) {                               <li>{project.tech}</li> }
-                                     </ul> 
-                                         <a href={project.link} target="_blank"> See more</a>
-         <a href={project.repo} target="_blank"> Repository</a>                             
-                                           </div>
-                                          </div>
-                                   
-              </>
-  )import './ProjectContent.css'
-import ImgProject from '../assets/img/${linguage}.svg'
-
-function ProjectContent({project}) {
-  return(
-    <>
-                                        <div class="container cardProject"> 
-                                 <div class="box-img"> 
-                                     <ImgProject/> 
-                                 </div> 
-                                 <div class="info-content"> 
-                                     <p class="h1 titleProject">{project.title}</p> 
-                                     <p class="h6 descriptionProject"> 
                                          {Project.description}
                                      </p> 
                                      <ul class="technologiesProject"> 
-                                     //fazer .map
+{/*                                      //fazer .map */}
                                        {/* {for(i=0;i>project.legth(); i++) {                               <li>{project.tech}</li> } */}
                                      </ul> 
                                          <a href={project.link} target="_blank"> See more</a>
@@ -48,10 +24,6 @@ function ProjectContent({project}) {
                                    
               </>
   )
-
-}
-
-export default ProjectContent
 
 }
 
